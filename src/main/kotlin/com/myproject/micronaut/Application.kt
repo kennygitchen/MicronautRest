240b9da6a1eb359013ef1197a5e99fe190855ef7
Kenny Chen <kc1230@hotmail.com>
2020-05-27 17:15:00 +1000

    * Project Creation * Added /rand endpoint

---
+++ src/main/kotlin/com/myproject/micronaut/Application.kt
@@ -0,0 +1,14 @@
+package com.myproject.micronaut
+
+import io.micronaut.runtime.Micronaut
+
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+                .packages("com.myproject.micronaut")
+                .mainClass(Application.javaClass)
+                .start()
+    }
+}
---
+++ src/main/kotlin/com/myproject/micronaut/rest/RandController.kt
@@ -0,0 +1,42 @@
+package com.myproject.micronaut.rest
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
+import kotlin.system.measureNanoTime
+
+
+/**
+ * This is a simple Rest endpoint that returns
+ * a random number
+ */
+@Controller("/rand")
+class RandController {
+    companion object {
+        val LOG: Logger = LoggerFactory.getLogger(RandController::class.java)
+    }
+
+    @Get("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    fun rand(): Double {
+        var random = 0.0
+        LOG.info("Request Received")
+        val taken = measureNanoTime {
+            random = genNum()
+        }
+        LOG.info("Request Finished, taken {}(ns)", taken)
+        return random
+    }
+
+    fun genNum(): Double {
+        var num = 0.0
+        for(n in 1..1000) {
+            num += Math.random()
+        }
+        return num
+    }
+}
