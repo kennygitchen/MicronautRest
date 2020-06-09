07a562825f8982f7921fab4024f9c37f467d6bd9
Kenny Chen <kc1230@hotmail.com>
2020-06-09 23:27:23 +1000

    test CI

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,3 +1,3 @@
 ./gradlew assemble
-native-image --no-server -J-Xmx8G -J-Xms2G -cp build/libs/MicronautRest-*-all.jar
+native-image --no-server  -J-Xmx10G -J-Xms8G -cp build/libs/MicronautRest-*-all.jar
 mv MicronautRest ./build/MicronautRest
