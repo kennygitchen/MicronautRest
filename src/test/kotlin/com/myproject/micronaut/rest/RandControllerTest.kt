f36ab2c94970a9024691c54318fbb137743b7f2a
Kenny Chen <kc1230@hotmail.com>
2020-06-09 15:19:57 +1000

    add kotlin unit tests

---
+++ src/test/kotlin/com/myproject/micronaut/rest/RandControllerTest.kt
@@ -0,0 +1,19 @@
+package com.myproject.micronaut.rest
+
+import io.kotlintest.shouldNotBe
+import io.kotlintest.specs.BehaviorSpec
+import io.micronaut.test.annotation.MicronautTest
+
+@MicronautTest
+class RandControllerTest(
+        private val restController: RandController
+) : BehaviorSpec({
+    given("the restContrller") {
+        `when`("the service is called with /rand") {
+            val result = restController.rand()
+            then("there should no error/excepton") {
+                result shouldNotBe null
+            }
+        }
+    }
+})
