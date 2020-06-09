60ac3c79fed43da952fea71d7524d3103d56c0a1
Kenny Chen <kc1230@hotmail.com>
2020-06-09 16:10:53 +1000

    test CI

--- src/test/kotlin/com/myproject/micronaut/rest/RandControllerTest.kt
+++ src/test/kotlin/com/myproject/micronaut/rest/RandControllerTest.kt
@@ -1,6 +1,6 @@
 package com.myproject.micronaut.rest
 
-import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.BehaviorSpec
 import io.micronaut.test.annotation.MicronautTest
 
@@ -12,7 +12,7 @@ class RandControllerTest(
         `when`("the service is called with /rand") {
             val result = restController.rand()
             then("there should no error/excepton") {
-                result shouldBe null
+                result shouldNotBe null
             }
         }
     }
